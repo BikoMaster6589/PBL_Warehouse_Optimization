e1680014a380dd7f93a481cf6dff0454f35f8561
Somesh Uniyal <162599177+someshuniyal31@users.noreply.github.com>
2025-06-15 20:27:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,17 @@
+// "Created postgres Db"
+const { Client } = require("pg");
+
+const db = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Warehouse_Db",
+  password: "mohan",
+  port: 5432,
+});
+
+db.connect();
+
+module.exports = db;
+
+
 
